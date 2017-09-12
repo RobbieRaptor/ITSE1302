be697ab8c592191e18d7b1dd4665991e81f95a8f
Robert Magallanes <31446178+RobbieRaptor@users.noreply.github.com>
2017-09-12 14:17:23 -0500

    Update Week2.groovy

--- Week2.groovy
+++ Week2.groovy
@@ -1,7 +1,10 @@
 // commit1 print Hello World on consol
-
   println "Hello World"
 
 //commit2 create two constants for your first and last name, use correct data type
   string firstName = "Robert"
   string lastName = "Magallanes"
+
+// commit3 create numaric variable assign it the value 1337, divide it be any number, assighn the outcome to a new variable.
+  num numericValue = 1337
+  set newVariable = numericValue / 7
