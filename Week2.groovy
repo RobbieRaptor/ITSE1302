5a3ee6b264e08c2c936e6b39dfded11e0ac2a0ab
Robert Magallanes <31446178+RobbieRaptor@users.noreply.github.com>
2017-09-12 14:25:51 -0500

    Update Week2.groovy

--- Week2.groovy
+++ Week2.groovy
@@ -8,3 +8,11 @@
 // commit3 create numaric variable assign it the value 1337, divide it be any number, assighn the outcome to a new variable.
   num numericValue = 1337
   set newVariable = numericValue / 7
+
+/** commit4 Using one or both of the variables create 5 different problems using the operands in this chapter.
+    Do not assighn new values to variables **/
+  num numericValue * 13 
+  num newVariable + 21
+  num numericValue - NewVariable + 12
+  num numericvalue / 13
+  num numericValue - 1330 + 7
