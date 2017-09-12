3cc4f01b5603742814fa2da3dd0d912746718a0d
Robert Magallanes <31446178+RobbieRaptor@users.noreply.github.com>
2017-09-12 13:58:34 -0500

    Update Week2.groovy

--- Week2.groovy
+++ Week2.groovy
@@ -1,3 +1,7 @@
-// print Hello World on consol
+// commit1 print Hello World on consol
 
-println "Hello World"
+  println "Hello World"
+
+//commit2 create two constants for your first and last name, use correct data type
+  string firstName = "Robert"
+  string lastName = "Magallanes"
