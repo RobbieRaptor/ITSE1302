ef47aa15e3cba3c3c1c5403f473fdd077febd9bb
Robert Magallanes <31446178+RobbieRaptor@users.noreply.github.com>
2017-09-18 22:00:01 -0500

    Hello world

--- Week2.groovy
+++ Week2.groovy
@@ -1,18 +1,43 @@
 // commit1 print Hello World on consol
-  println "Hello World"
+class  Week2{
+      static void main(String[] args){
+      
+            println ("Hello World");
 
 //commit2 create two constants for your first and last name, use correct data type
-  string firstName = "Robert"
-  string lastName = "Magallanes"
+        String firstName = "Robert";
+        String lastName = "Magallanes";
+
+        println ("My name is  "+firstName +lastName);
 
 // commit3 create numaric variable assign it the value 1337, divide it be any number, assighn the outcome to a new variable.
-  num numericValue = 1337
-  set newVariable = numericValue / 7
+        int x = 1337;
+        int y = x % 7;
+
+
 
 /** commit4 Using one or both of the variables create 5 different problems using the operands in this chapter.
-    Do not assighn new values to variables **/
-  num numericValue * 13 
-  num newVariable + 21
-  num numericValue - NewVariable + 12
-  num numericvalue / 13
-  num numericValue - 1330 + 7
+ Do not assighn new values to variables, print the answers **/
+        println (x * 13);
+        Println (y + 21);
+        Println (y % 2);
+        Println (x / 13);
+        Println (x - 1317);
+    
+// commit5 Print the values of all defined variables and constants to the console. (Hint: You should have four different items to display
+           println (firstName);
+           println (lastName);
+           println (x);   
+           println (y);
+// commit6 Use both multiline and single line comments to explain the following. Place each comment next to or directly above an example of what you are describing.
+        /**What is a constant?
+        a constant is a value/ variable that never changes
+        **/
+        //What is a Variable?
+            //a storage location paired with an identifier
+        //Explaine the difference between a constant and variable.
+        /** constant cannot be changed, a variable can change **/
+        //what is a data type?
+            // defines what kind of data youre working with
+      }
+    }
